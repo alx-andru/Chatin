1d9bcd4b50f0cf5029121870c1b8b4a3c0e70164
Alex <alex@sourceapp.io>
2017-11-29 08:53:32 -0500

    Test GitHub integration in VS

--- Chatin/App_Start/RouteConfig.cs
+++ Chatin/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Chatin
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            // useless comment
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
